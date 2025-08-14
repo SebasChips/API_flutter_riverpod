01cfbb72de0c6339c6e9c2a9d8b38d27e7cb115e
SebasChips <sebastianalarconbejar@gmail.com>
2025-08-14 10:35:27 -0600

    details page redirect

--- lib/details.dart
+++ lib/details.dart
@@ -1,70 +1,24 @@
 import 'package:flutter/material.dart';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'API riverpod web',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const DetailsPage(),
-    );
-  }
-}
-
-class CharacterRickAndMorty {
-  final int id;
-  final String name;
-  final String image;
-
-  CharacterRickAndMorty({
-    required this.id,
-    required this.name,
-    required this.image,
-  });
-
-  factory CharacterRickAndMorty.fromJson(Map<String, dynamic> json) {
-    return CharacterRickAndMorty(
-      id: json['id'],
-      name: json['name'],
-      image: json['image'],
-    );
-  }
-}
-
-Future<List<CharacterRickAndMorty>> fetchCharacters() async {
-  final response = await http.get(
-    Uri.parse('https://rickandmortyapi.com/api/character'),
-  );
-
-  if (response.statusCode == 200) {
-    final data = jsonDecode(response.body);
-    final List results = data['results'];
-    return results.map((e) => CharacterRickAndMorty.fromJson(e)).toList();
-  } else {
-    throw Exception('Error al cargar personajes');
-  }
-}
+import 'main.dart';
 
 class DetailsPage extends StatelessWidget {
-  const DetailsPage({super.key});
+  final CharacterRickAndMorty character;
+
+  const DetailsPage({super.key, required this.character});
 
   @override
   Widget build(BuildContext context) {
-    List <String> test = ["A0","B1"];
-
     return Scaffold(
-
+      appBar: AppBar(title: Text(character.name)),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 16),
+            Text('Origin: ${character.gender}'),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,49 +4,56 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
 import 'details.dart';
 
+
+
+//Inicialización de la app con riverpod
 void main() {
-  runApp(
-    const ProviderScope(
-      child: MyApp(),
-    ),
-  );
+  runApp(const ProviderScope(child: RickAndMorty()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+//Inicializa un widget estático para la demostración del contenido de la API
+class RickAndMorty extends StatelessWidget {
+  const RickAndMorty({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'API Riverpod Web',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
       ),
       home: const CharactersPage(),
     );
   }
 }
 
+//Clase del modelo de datos de la API
 class CharacterRickAndMorty {
   final int id;
   final String name;
   final String image;
+  final String species;
+  final String gender;
 
   CharacterRickAndMorty({
     required this.id,
     required this.name,
     required this.image,
+    required this.species,
+    required this.gender,
   });
 
+  //Mapeo de JSON a objeto "CharacterRickAndMorty"
   factory CharacterRickAndMorty.fromJson(Map<String, dynamic> json) {
     return CharacterRickAndMorty(
       id: json['id'],
       name: json['name'],
       image: json['image'],
+      species: json['species'],
+      gender: json['gender'],
     );
   }
 }
 
+//Obtención de datos de la API con función asíncrona
 Future<List<CharacterRickAndMorty>> fetchCharacters() async {
   final response = await http.get(
     Uri.parse('https://rickandmortyapi.com/api/character'),
@@ -67,31 +74,62 @@ final charactersProvider = FutureProvider<List<CharacterRickAndMorty>>((ref) {
 
 class CharactersPage extends ConsumerWidget {
   const CharactersPage({super.key});
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final charactersAsync = ref.watch(charactersProvider);
-
     return Scaffold(
-      appBar: AppBar(title: const Text('Rick and Morty')),
+      appBar: AppBar(
+        title: const Text(
+          'Personajes Rick & Morty',
+          style: TextStyle(fontSize: 35),
+        ),
+      ),
       body: charactersAsync.when(
         data: (characters) => ListView.builder(
           itemCount: characters.length,
           itemBuilder: (context, index) {
-            final c = characters[index];
-            return ListTile(
-              leading: Image.network(c.image),
-              title: Text(c.name),
-              trailing: ElevatedButton(
-                child: const Text('Open route'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const DetailsPage(),
+            var c = characters[index];
+            return Card(
+              margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Image.network(
+                      c.image,
+                      width: 150,
+                      height: 200,
+                      fit: BoxFit.fill,
                     ),
-                  );
-                },
+                    const SizedBox(width: 16),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            c.name,
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => DetailsPage(character: c),
+                                ),
+                              );
+                            },
+                            child: const Text('Descripción'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             );
           },
